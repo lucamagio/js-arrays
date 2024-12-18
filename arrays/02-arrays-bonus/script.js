faf0885ba8772a913d44a56aaa764bb69793ece2
Lucamagio <lucamagionami4@gmail.com>
2024-12-18 18:28:16 +0100

    ex. Array 02

--- arrays/02-arrays-bonus/script.js
+++ arrays/02-arrays-bonus/script.js
@@ -10,10 +10,22 @@ const teachers = [
 
 // 1. Inverti l'ordine degli insegnanti nell'array teachers
 // e salva il risultato nella variabile reversedTeachers
-const reversedTeachers = null;
-
+let reversedTeachers = [];
+for(i = teachers.length -1; i >= 0; i--){
+    let teach = teachers[i]
+    reversedTeachers.push(teach)
+}
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
-const longNames = null;
+let longNames = []
+for(i = 0; i < teachers.length; i++){
+  let teach = teachers[i]
+  if (teach.length >= 5){
+    longNames.push(teach)
+  }
+}
 
 // 3. Rimuovi 'Ed' dall'array teachers
+
+teachers.splice(1, 1)
+console.log(teachers)
